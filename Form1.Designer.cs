ea9ae3977aa249415cd97b22f03e3ac9f34ecc41
Erwin <Erwin@ERWIN-PC>
2021-12-12 16:54:23 +0100

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace goblinRevolver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lvwDevices = new System.Windows.Forms.ListView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(336, 837);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Refresh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lvwDevices
+            // 
+            this.lvwDevices.GridLines = true;
+            this.lvwDevices.HideSelection = false;
+            this.lvwDevices.LabelEdit = true;
+            this.lvwDevices.Location = new System.Drawing.Point(13, 13);
+            this.lvwDevices.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lvwDevices.Name = "lvwDevices";
+            this.lvwDevices.Size = new System.Drawing.Size(1529, 798);
+            this.lvwDevices.TabIndex = 1;
+            this.lvwDevices.UseCompatibleStateImageBehavior = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(751, 837);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 28);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Disable";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.disable);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1061, 837);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(108, 28);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Enable";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.enable);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1557, 887);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lvwDevices);
+            this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListView lvwDevices;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace goblinRevolver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lvwDevices.View = View.Details;
+            lvwDevices.GridLines = true;
+            lvwDevices.FullRowSelect = true;
+
+            lvwDevices.Columns.Add("DeviceID", 390);
+            lvwDevices.Columns.Add("PNPDeviceID", 390);
+            lvwDevices.Columns.Add("Description", 350);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            lvwDevices.Items.Clear();
+
+            var usbDevices = GetUSBDevices();
+
+            foreach (var usbDevice in usbDevices)
+            {
+                //Console.WriteLine("Device ID: {0}, PNP Device ID: {1}, Description: {2}", usbDevice.DeviceID, usbDevice.PnpDeviceID, usbDevice.Description);
+
+          
+
+                //Add items in the listview
+                string[] arr = new string[4];
+                ListViewItem itm;
+
+                //Add first item
+                arr[0] = usbDevice.DeviceID.ToString();
+                arr[1] = usbDevice.PnpDeviceID.ToString();
+                arr[2] = usbDevice.Description.ToString();
+                itm = new ListViewItem(arr);
+                lvwDevices.Items.Add(itm);
+
+                /*
+                ListViewItem new_item = lvwDevices.Items.Add(
+                    usbDevice.DeviceID.ToString());
+                new_item.SubItems.Add(
+                    usbDevice.PnpDeviceID.ToString());
+                new_item.SubItems.Add(
+                    usbDevice.Description.ToString());
+                */
+            }
+
+            //Console.Read();
+
+            /*
+            ManagementObjectSearcher device_searcher =
+            new ManagementObjectSearcher("SELECT * FROM Win32_USBHub");
+            foreach (ManagementObject usb_device in device_searcher.Get())
+            {
+                ListViewItem new_item = lvwDevices.Items.Add(
+                    usb_device.Properties["DeviceID"].Value.ToString());
+                new_item.SubItems.Add(
+                    usb_device.Properties["PNPDeviceID"].Value.ToString());
+                new_item.SubItems.Add(
+                    usb_device.Properties["Description"].Value.ToString());
+            }
+            */
+        }
+
+        static List<ManagementBaseObject> GetLogicalDevices()
+        {
+            List<ManagementBaseObject> devices = new List<ManagementBaseObject>();
+            ManagementObjectCollection collection;
+            using (var searcher = new ManagementObjectSearcher("root\\CIMV2",
+                                  @"Select * From CIM_LogicalDevice"))
+                collection = searcher.Get();
+            foreach (var device in collection)
+            {
+                devices.Add(device);
+            }
+            collection.Dispose();
+            return devices;
+        }
+
+
+        static List<USBDeviceInfo> GetUSBDevices()
+        {
+            List<USBDeviceInfo> devices = new List<USBDeviceInfo>();
+
+            ManagementObjectCollection collection;
+            using (var searcher = new ManagementObjectSearcher(@"Select * From Win32_USBHub"))
+                collection = searcher.Get();
+
+            foreach (var device in collection)
+            {
+                devices.Add(new USBDeviceInfo(
+                (string)device.GetPropertyValue("DeviceID"),
+                (string)device.GetPropertyValue("PNPDeviceID"),
+                (string)device.GetPropertyValue("Description")
+                ));
+            }
+
+            collection.Dispose();
+            return devices;
+        }
+
+        class USBDeviceInfo
+        {
+            public USBDeviceInfo(string deviceID, string pnpDeviceID, string description)
+            {
+                this.DeviceID = deviceID;
+                this.PnpDeviceID = pnpDeviceID;
+                this.Description = description;
+            }
+            public string DeviceID { get; private set; }
+            public string PnpDeviceID { get; private set; }
+            public string Description { get; private set; }
+        }
+
+        private void disable(object sender, EventArgs e)
+        {
+            string deviceID = lvwDevices.SelectedItems[0].Text;
+            Console.WriteLine(deviceID);
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "C:\\repo\\devcon\\devcon64.exe"; 
+            startInfo.Arguments = "disable \"@" + deviceID + "\"";
+            Console.WriteLine(startInfo.Arguments); 
+            startInfo.Verb = "runas";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        private void enable(object sender, EventArgs e)
+        {
+            string deviceID = lvwDevices.SelectedItems[0].Text;
+            Console.WriteLine(deviceID);
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "C:\\repo\\devcon\\devcon64.exe enable \"@" + deviceID + "\"";
+            Console.WriteLine(startInfo.Arguments);
+            startInfo.Verb = "runas";
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+    }
+}
