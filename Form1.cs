4e44414b78852173efc49b56d7e3bea3b4d84a17
GodfatherKK <kocak.kubilay@web.de>
2021-12-22 19:25:42 +0100

    added exception handling and exposure

--- Form1.cs
+++ Form1.cs
@@ -12,30 +12,60 @@ namespace goblinRevolver
 
     public partial class Form1 : Form
     {
-        // INIT
+
+        // ######### INIT #########################################################################################
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        // ########################################################################################################
+
 
 
         // ######### EVENTS #######################################################################################
 
         // ONLOAD HOOK
         private void Form1_Load(object sender, EventArgs e)
-        {
-            lvwDevices.View = View.Details;
-            lvwDevices.GridLines = true;
-            lvwDevices.FullRowSelect = true;
+            {
+                try
+                {
+                    lvwDevices.View = View.Details;
+                    lvwDevices.GridLines = true;
+                    lvwDevices.FullRowSelect = true;
 
-            lvwDevices.Columns.Add("DeviceID", 390);
-            lvwDevices.Columns.Add("PNPDeviceID", 390);
-            lvwDevices.Columns.Add("Description", 350);
+                    lvwDevices.Columns.Add("DeviceID", 390);
+                    lvwDevices.Columns.Add("PNPDeviceID", 390);
+                    lvwDevices.Columns.Add("Description", 350);
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show("Exception: " + exp.Message);
+                }
 
-            OS_bit.Text = getIs64BitOSText();
 
-            update_USBDevicesTable();
-        }
+                try
+                {
+                    OS_bit.Text = getIs64BitOSText();
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show("Exception: " + exp.Message);
+                }
+
+
+                try
+                {
+                    update_USBDevicesTable();
+                }
+                catch (Exception exp)
+                {
+                    MessageBox.Show("Exception: " + exp.Message);
+                }
+
+        
+            }
 
         // ########################################################################################################
 
@@ -44,49 +74,65 @@ namespace goblinRevolver
 
         // ######### USB ##########################################################################################        
 
-
         // UPDATE USB DEVICES TABLE
         private void update_USBDevicesTable()
         {
-            lvwDevices.Items.Clear();
-
-            var usbDevices = GetUSBDevices();
-
-            foreach (var usbDevice in usbDevices)
+            try
             {
-                //Add items in the listview
-                string[] arr = new string[4];
-                ListViewItem itm;
+                lvwDevices.Items.Clear();
 
-                //Add first item
-                arr[0] = usbDevice.DeviceID.ToString();
-                arr[1] = usbDevice.PnpDeviceID.ToString();
-                arr[2] = usbDevice.Description.ToString();
-                itm = new ListViewItem(arr);
-                lvwDevices.Items.Add(itm);
+                var usbDevices = GetUSBDevices();
+
+                foreach (var usbDevice in usbDevices)
+                {
+                    //Add items in the listview
+                    string[] arr = new string[4];
+                    ListViewItem itm;
+
+                    //Add first item
+                    arr[0] = usbDevice.DeviceID.ToString();
+                    arr[1] = usbDevice.PnpDeviceID.ToString();
+                    arr[2] = usbDevice.Description.ToString();
+                    itm = new ListViewItem(arr);
+                    lvwDevices.Items.Add(itm);
+                }
             }
+            catch (Exception exp)
+            {
+                MessageBox.Show("Exception: " + exp.Message);
+            }
+            
         }
 
         // GET USB DEVICES
         static List<USBDeviceInfo> GetUSBDevices()
         {
-            List<USBDeviceInfo> devices = new List<USBDeviceInfo>();
-
-            ManagementObjectCollection collection;
-            using (var searcher = new ManagementObjectSearcher(@"Select * From Win32_USBHub"))
-                collection = searcher.Get();
-
-            foreach (var device in collection)
+            try
             {
-                devices.Add(new USBDeviceInfo(
-                (string)device.GetPropertyValue("DeviceID"),
-                (string)device.GetPropertyValue("PNPDeviceID"),
-                (string)device.GetPropertyValue("Description")
-                ));
+                List<USBDeviceInfo> devices = new List<USBDeviceInfo>();
+
+                ManagementObjectCollection collection;
+                using (var searcher = new ManagementObjectSearcher(@"Select * From Win32_USBHub"))
+                    collection = searcher.Get();
+
+                foreach (var device in collection)
+                {
+                    devices.Add(new USBDeviceInfo(
+                    (string)device.GetPropertyValue("DeviceID"),
+                    (string)device.GetPropertyValue("PNPDeviceID"),
+                    (string)device.GetPropertyValue("Description")
+                    ));
+                }
+
+                collection.Dispose();
+                return devices;
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show("Exception: " + exp.Message);
             }
 
-            collection.Dispose();
-            return devices;
+            return null;
         }
 
         // TYPE: USBDeviceInfo
@@ -106,59 +152,83 @@ namespace goblinRevolver
         // BUTTON: DISABLE USB
         private void btn_disableUSB(object sender, EventArgs e)
         {
-            if (lvwDevices.SelectedItems[0].Text != null)
+            try
             {
-                string deviceID = lvwDevices.SelectedItems[0].Text;
-                System.Diagnostics.Process process = new System.Diagnostics.Process();
-                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-                if (getIs64BitOS())
+                if (lvwDevices.SelectedItems[0].Text != null)
                 {
-                    startInfo.FileName = "devcon64.exe";
+                    string deviceID = lvwDevices.SelectedItems[0].Text;
+                    System.Diagnostics.Process process = new System.Diagnostics.Process();
+                    System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                    startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                    if (getIs64BitOS())
+                    {
+                        startInfo.FileName = "devcon64.exe";
+                    }
+                    else
+                    {
+                        startInfo.FileName = "devcon.exe";
+                    }
+                    startInfo.Arguments = "disable \"@" + deviceID + "\"";
+                    startInfo.Verb = "runas";
+                    process.StartInfo = startInfo;
+                    process.Start();
                 }
-                else
-                {
-                    startInfo.FileName = "devcon.exe";
-                }                    
-                startInfo.Arguments = "disable \"@" + deviceID + "\"";
-                startInfo.Verb = "runas";
-                process.StartInfo = startInfo;
-                process.Start();
             }
+            catch (Exception exp)
+            {
+                MessageBox.Show("Exception: " + exp.Message);
+            }
+
+            
         }
 
         // BUTTON: ENABLE USB
         private void btn_enableUSB(object sender, EventArgs e)
         {
-            if (lvwDevices.SelectedItems[0].Text != null)
+            try
             {
-                string deviceID = lvwDevices.SelectedItems[0].Text;
-                System.Diagnostics.Process process = new System.Diagnostics.Process();
-                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-                if (getIs64BitOS())
+                if (lvwDevices.SelectedItems[0].Text != null)
                 {
-                    startInfo.FileName = "devcon64.exe";
+                    string deviceID = lvwDevices.SelectedItems[0].Text;
+                    System.Diagnostics.Process process = new System.Diagnostics.Process();
+                    System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                    startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                    if (getIs64BitOS())
+                    {
+                        startInfo.FileName = "devcon64.exe";
+                    }
+                    else
+                    {
+                        startInfo.FileName = "devcon.exe";
+                    }
+                    startInfo.Arguments = "enable \"@" + deviceID + "\"";
+                    startInfo.Verb = "runas";
+                    process.StartInfo = startInfo;
+                    process.Start();
                 }
-                else
-                {
-                    startInfo.FileName = "devcon.exe";
-                }
-                startInfo.Arguments = "enable \"@" + deviceID + "\"";
-                startInfo.Verb = "runas";
-                process.StartInfo = startInfo;
-                process.Start();
             }
+            catch (Exception exp)
+            {
+                MessageBox.Show("Exception: " + exp.Message);
+            }
+
+            
 
         }
 
         // BUTTON: REFRESH USB DEVICES
         private void btn_refreshUSBDevices(object sender, EventArgs e)
         {
-            update_USBDevicesTable();
+            try
+            {
+                update_USBDevicesTable();
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show("Exception: " + exp.Message);
+            }
+            
         }
-
-
 
         // ########################################################################################################
 
@@ -170,27 +240,43 @@ namespace goblinRevolver
         // OPEN GOBLIN COMPILER
         private void openCompiler(object sender, EventArgs e)
         {
-            System.Diagnostics.Process process = new System.Diagnostics.Process();
-            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-            startInfo.FileName = "Compiler.exe";
-            startInfo.Arguments = "";
-            startInfo.Verb = "runas";
-            process.StartInfo = startInfo;
-            process.Start();
+            try
+            {
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startInfo.FileName = "Compiler.exe";
+                startInfo.Arguments = "";
+                startInfo.Verb = "runas";
+                process.StartInfo = startInfo;
+                process.Start();
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show("Exception: " + exp.Message);
+            }
+        
         }
 
         // OPEN GOBLIN UPLOADER
         private void openUploader(object sender, EventArgs e)
         {
-            System.Diagnostics.Process process = new System.Diagnostics.Process();
-            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
-            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-            startInfo.FileName = "Uploader.exe";
-            startInfo.Arguments = "";
-            startInfo.Verb = "runas";
-            process.StartInfo = startInfo;
-            process.Start();
+            try
+            {
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+                startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+                startInfo.FileName = "Uploader.exe";
+                startInfo.Arguments = "";
+                startInfo.Verb = "runas";
+                process.StartInfo = startInfo;
+                process.Start();
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show("Exception: " + exp.Message);
+            }
+        
         }
 
         // ########################################################################################################
@@ -200,29 +286,48 @@ namespace goblinRevolver
 
         // ######### ENVIRONMENT ##################################################################################
 
-
         // CHECK IF OS IS 64-BIT
         private bool getIs64BitOS() 
         {
-            return Environment.Is64BitOperatingSystem;
+            try
+            {
+                return Environment.Is64BitOperatingSystem;
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show("Exception: " + exp.Message);
+            }
+
+            // default to 64 Bit 
+            return true; // ToDo: build enumaration
+        
         }
 
 
         // GET TEXT FOR IS 64-BIT OS LABEL
         private string getIs64BitOSText()
         {
-            bool is64Bit = getIs64BitOS();
+            try
+            {
+                bool is64Bit = getIs64BitOS();
 
-            if (is64Bit)
-            { 
-                return "64 Bit System"; 
+                if (is64Bit)
+                {
+                    return "64 Bit System";
+                }
+                else
+                {
+                    return "32 Bit System";
+                }
+                return "";
             }
-            else
-            { 
-                return "32 Bit System"; 
+            catch (Exception exp)
+            {
+                MessageBox.Show("Exception: " + exp.Message);
             }
+
+            // default to empty string
             return "";
-
         }
 
         // ########################################################################################################
